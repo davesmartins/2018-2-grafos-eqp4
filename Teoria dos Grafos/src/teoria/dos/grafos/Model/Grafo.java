34de12e3cb5164d23d0137f29599a1791d590677
gabetto <gabrielbeto83@gmail.com>
2018-11-27 21:10:06 -0200

    programa funcionando, bugs corrigidos e novos bugs futuros implementados

--- Teoria dos Grafos/src/teoria/dos/grafos/Model/Grafo.java
+++ Teoria dos Grafos/src/teoria/dos/grafos/Model/Grafo.java
@@ -296,103 +296,36 @@ public class Grafo {
         return 0;
     }
 
-//    public void coloreGrafo() {
-//        int cor = 0;
-//        int indice = 0;
-//
-//        for (int i = 0; i < vertices.size(); i++) {
-//            for (int j = 0; j < vertices.get(i).ligacoes.size(); j++) {
-//                int corFilho = 0;
-//                for (int k = 0; k <= cor; k++) {
-//                    if (vertices.get(i).getCor() == 0 && vertices.get(i).getCor() == cor ){
-//                        corFilho = cor + 1;
-//                    }
-//                    if (vertices.get(i).getCor() != k) {
-//                        corFilho = k;
-//                        break;
-//                    }
-//                }
-//
-//                vertices.get(i).ligacoes.get(j).getDestino().setCor(corFilho);
-//                indice = getIndiceVertice(vertices.get(i).ligacoes.get(j).getDestino().getNome());
-//                vertices.get(indice).setCor(corFilho);
-//
-//                if (corFilho == cor) {
-//                    cor++;
-//                }
-//            }
-//        }
-//    }
-//    public void coloreGrafo() {
-//        int cor = 0;
-//
-//        Collections.sort(vertices, new VerticeComparator());
-//        for (Vertice verticeAtual : vertices) {
-//            for (Aresta ligacaoVerticeAtual : verticeAtual.getLigacoes()) {
-//
-//                if (verticeAtual.getCor() == ligacaoVerticeAtual.getDestino().getCor() && verticeAtual.getCor() == cor) {
-//                    cor++;
-//                }
-//
-//                int corFilho = 0;
-//                for (int i = 0; i <= cor; i++) {
-//                    if (verticeAtual.getCor() != i) {
-//                        corFilho = i;
-//                        for (Aresta ligacoes : arestas) {
-//                            if (ligacoes.getDestino().getNome() == ligacaoVerticeAtual.getDestino().getNome() && ligacoes.getDestino().getCor() == i) {
-//                                corFilho = -1;
-//                            }
-//                            if (ligacoes.getOrigem().getNome() == ligacaoVerticeAtual.getDestino().getNome() && ligacoes.getOrigem().getCor() == i) {
-//                                corFilho = -1;
-//                            }
-//                        }
-//                        if (corFilho >= 0) {
-//                            break;
-//                        }
-//                    }
-//                }
-//                ligacaoVerticeAtual.getDestino().setCor(corFilho);
-//                int indice = getIndiceVertice(ligacaoVerticeAtual.getDestino().getNome());
-//                vertices.get(indice).setCor(corFilho);
-//            }
-//        }
-//    }
     public void coloreGrafo() {
-       
+
         ArrayList<Vertice> listaVertices = getVertices();
-        ArrayList<Vertice> listaVerticesColoridos = new ArrayList();
 
         Collections.sort(listaVertices, new VerticeComparator());
 
-        for (Vertice vertices : listaVertices) {
-            listaVerticesColoridos.add(coloreVizinhos(vertices));
-        }
-
-        this.setVertices(listaVerticesColoridos);
-
-    }
-    
-    private void coloreVizinhos(Vertice vertice){
-        ArrayList<Vertice> vizinhos = getVizinhos(vertice);
-        
-        int corInicial = vertice.getCor();
-        
-        for(Vertice v : vizinhos){
-            if(v.getCor() == corInicial){
-                v.setCor(corInicial + 1);
+        for (Vertice vertice : listaVertices) {
+            for(Vertice v : getVizinhos(vertice)){
+                if(v.getCor() == vertice.getCor()){
+                    v.setCor(vertice.getCor()+1);
+                }
             }
         }
+
+
     }
 
-   
     public Boolean isVizinho(Vertice a, Vertice b) {
-        if (a.getLigacoes().contains(b)) {
-            return true;
-        } else if (b.getLigacoes().contains(a)) {
-            return true;
-        } else {
-            return false;
+        for(Aresta aresta : a.getLigacoes()){
+            if(aresta.getDestino().getNome().equals(b.getNome())){
+                return true;
+            }
         }
+        for(Aresta aresta : b.getLigacoes()){
+            if(aresta.getDestino().getNome().equals(a.getNome())){
+                return true;
+            }
+        }
+        
+        return false;
     }
 
     public ArrayList<Vertice> getVizinhos(Vertice vertice) {
@@ -408,8 +341,6 @@ public class Grafo {
         }
 
         return listaVizinhos;
-
     }
-
 
 }
